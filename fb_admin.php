80d3ee8596a0c19942cb81652f4c3bee75d5dc94
unknown <zalemmm@yahoo.com"\n>
2017-03-03 14:18:40 +0100

    !!! a corriger en prod fb_admin mise en place de nicEdit en local

--- fb_admin.php
+++ fb_admin.php
@@ -2260,7 +2260,7 @@ if (isset($_POST['fb_editmail'])) {
 	$simplemail = $wpdb->get_row("SELECT * FROM `$fb_tablename_mails` WHERE id = '$ident'");
 	echo '<form name="editmail" id="editmail" action="" method="post"><input type="hidden" name="editmail" value="'.$simplemail->id.'" />';
 	echo '<p>Topic: <input type="text" name="nmail_topic" value="'.stripslashes($simplemail->topic).'" /></p>';
-	echo '<script type="text/javascript" src="http://js.nicedit.com/nicEdit-latest.js"></script>
+	echo '<script type="text/javascript" src="https://dev.france-banderole.com/wp-content/plugins/fbshop/js/nicEdit-latest.js"></script>
 			<script type="text/javascript">
 				bkLib.onDomLoaded(function() {
 					new nicEditor().panelInstance(\'incon\');
@@ -2275,7 +2275,7 @@ if (isset($_POST['fb_editmail'])) {
 	echo '<div id="poststuff" class="metabox-holder has-right-sidebar"><div class="postbox"><h3><span>Add new:</span></h3><div class="inside">';
 	echo '<form name="newmail" id="newmail" action="" method="post"><input type="hidden" name="addmail" />';
 	echo '<p>Topic: <input type="text" name="mail_topic" /></p>';
-	echo '<script type="text/javascript" src="http://js.nicedit.com/nicEdit-latest.js"></script>
+	echo '<script type="text/javascript" src="https://dev.france-banderole.com/wp-content/plugins/fbshop/js/nicEdit-latest.js"></script>
 			<script type="text/javascript">
 				bkLib.onDomLoaded(function() {
 					new nicEditor().panelInstance(\'incon\');
@@ -2365,7 +2365,7 @@ if (isset($_POST['fb_editmail'])) {
 	$simplemail = $wpdb->get_row("SELECT * FROM `$fb_tablename_topic` WHERE id = '$ident'");
 	echo '<form name="editmail" id="editmail" action="" method="post"><input type="hidden" name="editmail" value="'.$simplemail->id.'" />';
 	echo '<p>Topic: <input type="text" name="nmail_topic" value="'.stripslashes($simplemail->topic).'" /></p>';
-	echo '<script type="text/javascript" src="http://js.nicedit.com/nicEdit-latest.js"></script>
+	echo '<script type="text/javascript" src="https://dev.france-banderole.com/wp-content/plugins/fbshop/js/nicEdit-latest.js"></script>
 			<script type="text/javascript">
 				bkLib.onDomLoaded(function() {
 					new nicEditor().panelInstance(\'incon\');
@@ -2381,7 +2381,7 @@ if (isset($_POST['fb_editmail'])) {
 	echo '<div id="poststuff" class="metabox-holder has-right-sidebar"><div class="postbox"><h3><span>Add new:</span></h3><div class="inside">';
 	echo '<form name="newmail" id="newmail" action="" method="post"><input type="hidden" name="addmail" />';
 	echo '<p>Topic: <input type="text" name="mail_topic" /></p>';
-	echo '<script type="text/javascript" src="http://js.nicedit.com/nicEdit-latest.js"></script>
+	echo '<script type="text/javascript" src="https://dev.france-banderole.com/wp-content/plugins/fbshop/js/nicEdit-latest.js"></script>
 			<script type="text/javascript">
 				bkLib.onDomLoaded(function() {
 					new nicEditor().panelInstance(\'incon\');
@@ -3344,14 +3344,14 @@ traitement_passage_cloture($number,$fb_tablename_order,$fb_tablename_topic,$fb_t
 	?>
 
 
-		<script type="text/javascript" src="http://js.nicedit.com/nicEdit-latest.js"></script>
+		<script type="text/javascript" src="https://dev.france-banderole.com/wp-content/plugins/fbshop/js/nicEdit-latest.js"></script>
 			<script type="text/javascript">
 				bkLib.onDomLoaded(function() {
 					new nicEditor().panelInstance('incon2');
 				});
 			</script>
 
-
+      
 	<?php
 
 
