f830175b04d23be9827d0e5a65649326239872c1
unknown <zalemmm@yahoo.com"\n>
2017-04-10 10:28:08 +0200

    indentation fichier

--- prod_pages/cartes.php
+++ prod_pages/cartes.php
@@ -75,23 +75,44 @@
 
       <li id="id_101" class="form-line optionsformlineOld">
         <span class="title">OPTIONS DE LIVRAISON <span class="splitorhide">DISPONIBLES :</span> </span>
-        <span class="options_single">
-          <span class="optionsright"><label class="form-label-left" id="label_etiquette" for="etiquette">Retrait Colis a L'Atelier</label><input type="checkbox" class="form-checkbox" id="etiquette" name="etiquette[]" value="" onchange="JKakemono.czyscpola(); " /><span class="helpButton" onmouseover="pokazt('helpTextetiquette');" onmouseout="ukryjt('helpTextetiquette');"><span class="helpText" id="helpTextetiquette" style="visibility:hidden;">Retrait de votre commande à l'atelier de Vitrolles. Vos frais de port seront supprimés de votre devis avant votre paiement.</span></span></span>
-          <span class="optionsleft"><label class="form-label-left" id="label_rush24" for="rush24">Délai Rush 24/48H</label><input type="checkbox" class="form-checkbox" id="rush24" name="rush24[]" value="" onchange="JKakemono.rushcheckbox24(); JKakemono.czyscpola(); " /><span class="helpButton" onmouseover="pokazt('helpTextRush24');" onmouseout="ukryjt('helpTextRush24');"><span class="helpText" id="helpTextRush24" style="visibility:hidden;">Pour toute commande passée et réglée avant midi du lundi au jeudi, le colis sera livré le lendemain ou surlendemain avant 13h00 par TNT Express à l’adresse indiquée par le client.</span></span></span>
-        </span>
 
         <span class="options_single">
-          <span class="optionsright"><label class="form-label-left" id="label_colis" for="colis">Colis revendeur</label><input type="checkbox" class="form-checkbox" id="colis" name="colis[]" value="" onchange="JKakemono.colisRevendeurcheckbox(); JKakemono.czyscpola(); " /><span class="helpButton" onmouseover="pokazt('helpTextcolis');" onmouseout="ukryjt('helpTextcolis');"><span class="helpText" id="helpTextcolis" style="visibility:hidden;">Vous permet d’avoir une expédition neutre sans étiquetage France banderole.</span></span></span>
-          <!--<span class="optionsright"><label class="form-label-left" id="label_rush72" for="rush72">Délai Rush 72H</label><input type="checkbox" class="form-checkbox" id="rush72" name="rush72[]" value="" onchange="JKakemono.rushcheckbox72(); JKakemono.czyscpola(); " /><span class="helpButton" onmouseover="pokazt('helpTextrush72');" onmouseout="ukryjt('helpTextrush72');"><span class="helpText" id="helpTextrush72" style="visibility:hidden;">Pour toute commande passée et réglée avant midi du lundi au jeudi, le colis sera livré 72H après par TNT Express à l’adresse indiquée par le client.</span></span></span> -->
-        </span>
 
-        <span class="options_single">
-          <span class="optionsleft"><label class="form-label-left" id="label_economique" for="economique">Délai économique 6 à 8 jours ouvrés</label><input type="checkbox" class="form-checkbox" id="economique" name="economique[]" value="" onchange="JKakemono.rushcheckboxeconomique();; JKakemono.czyscpola(); "><span class="helpButton" onmouseover="pokazt('helpTextRush72');" onmouseout="ukryjt('helpTextRush72');"><span class="helpText" id="helpTextRush72" style="visibility: hidden;">Vous n’êtes pas pressé et souhaitez économiser 30%, vos délais de livraison seront de 6 à 8 jours ouvrés.</span></span></span>
-          <span class="optionsright"><label class="form-label-left" id="label_relais" for="relais">Dépot en relais colis</label><input type="checkbox" class="form-checkbox" id="relais" name="relais[]" value="" onchange="JKakemono.relaisColischeckbox(); JKakemono.czyscpola(); " /><span class="helpButton" onmouseover="pokazt('helpTextrelais');" onmouseout="ukryjt('helpTextrelais');"><span class="helpText" id="helpTextrelais" style="visibility:hidden;">Vous ne souhaitez pas être livré à une adresse professionnelle ou personnelle. Votre commande sera déposée dans le relais colis le plus proche de l adresse souhaitée. Vous serez informé du nom et de l adresse du point de dépot dans votre accès client la veille de l expedition.</span></span></span>
-        </span>
+          <span class="optionsright"><label class="form-label-left" id="label_etiquette" for="etiquette">Retrait Colis a L'Atelier</label>
+            <input type="checkbox" class="form-checkbox" id="etiquette" name="etiquette[]" value="" onchange="JKakemono.czyscpola(); " />
+            <span class="helpButton" onmouseover="pokazt('helpTextetiquette');" onmouseout="ukryjt('helpTextetiquette');">
+              <span class="helpText" id="helpTextetiquette" style="visibility:hidden;">Retrait de votre commande à l'atelier de Vitrolles. Vos frais de port seront supprimés de votre devis avant votre paiement.</span>
+            </span>
+          </span>
 
+          <span class="optionsleft"><label class="form-label-left" id="label_rush24" for="rush24">Délai Rush 24/48H</label>
+            <input type="checkbox" class="form-checkbox" id="rush24" name="rush24[]" value="" onchange="JKakemono.rushcheckbox24(); JKakemono.czyscpola(); " />
+            <span class="helpButton" onmouseover="pokazt('helpTextRush24');" onmouseout="ukryjt('helpTextRush24');">
+              <span class="helpText" id="helpTextRush24" style="visibility:hidden;">Pour toute commande passée et réglée avant midi du lundi au jeudi, le colis sera livré le lendemain ou surlendemain avant 13h00 par TNT Express à l’adresse indiquée par le client.</span>
+            </span>
+          </span>
 
-        <span class="options_single">
+          <span class="optionsright"><label class="form-label-left" id="label_colis" for="colis">Colis revendeur</label>
+            <input type="checkbox" class="form-checkbox" id="colis" name="colis[]" value="" onchange="JKakemono.colisRevendeurcheckbox(); JKakemono.czyscpola(); " />
+            <span class="helpButton" onmouseover="pokazt('helpTextcolis');" onmouseout="ukryjt('helpTextcolis');">
+              <span class="helpText" id="helpTextcolis" style="visibility:hidden;">Vous permet d’avoir une expédition neutre sans étiquetage France banderole.</span>
+            </span>
+          </span>
+
+          <span class="optionsleft"><label class="form-label-left" id="label_economique" for="economique">Délai économique 6 à 8 jours ouvrés</label>
+            <input type="checkbox" class="form-checkbox" id="economique" name="economique[]" value="" onchange="JKakemono.rushcheckboxeconomique();; JKakemono.czyscpola(); ">
+            <span class="helpButton" onmouseover="pokazt('helpTextRush72');" onmouseout="ukryjt('helpTextRush72');">
+              <span class="helpText" id="helpTextRush72" style="visibility: hidden;">Vous n’êtes pas pressé et souhaitez économiser 30%, vos délais de livraison seront de 6 à 8 jours ouvrés.</span>
+            </span>
+          </span>
+
+          <span class="optionsright"><label class="form-label-left" id="label_relais" for="relais">Dépot en relais colis</label>
+            <input type="checkbox" class="form-checkbox" id="relais" name="relais[]" value="" onchange="JKakemono.relaisColischeckbox(); JKakemono.czyscpola(); " />
+            <span class="helpButton" onmouseover="pokazt('helpTextrelais');" onmouseout="ukryjt('helpTextrelais');">
+              <span class="helpText" id="helpTextrelais" style="visibility:hidden;">Vous ne souhaitez pas être livré à une adresse professionnelle ou personnelle. Votre commande sera déposée dans le relais colis le plus proche de l adresse souhaitée. Vous serez informé du nom et de l adresse du point de dépot dans votre accès client la veille de l expedition.</span>
+            </span>
+          </span>
+
           <span class="optionsleft">
             <label class="form-label-left" id="label_fedex" for="fedex">Livraison gratuite 7 à 9 jours</label>
             <input type="checkbox" class="form-checkbox" id="fedex" name="fedex[]" value="" onchange=" JKakemono.czyscpola(); " />
@@ -99,6 +120,7 @@
               <span class="helpText" id="helpTextfedex" style="visibility:hidden;">Livraison gratuite en 7 à 9 jours ouvrés (non compatible avec les délais Rush, et Relais colis).</span>
             </span>
           </span>
+
           <span class="optionsright">
             <label class="form-label-left" id="label_tnt" for="tnt">Livraison payante 4 à 7 jours</label>
             <input type="checkbox" class="form-checkbox" id="tnt" name="tnt[]" value="" onchange=" JKakemono.czyscpola(); " />
@@ -106,7 +128,9 @@
               <span class="helpText" id="helpTexttnt" style="visibility:hidden;">Livraison payante en 4 à 7 jours ouvrés (non compatible avec un colis hors-norme*)</span>
             </span>
           </span>
+
         </span>
+
       </li>
 
       <li class="form-line" id="id_26a" style="margin-top: 33px;">
